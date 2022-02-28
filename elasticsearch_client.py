1c9c0672d0d3896b3dc0e835dcc1b985e765da59
Alex Anoshkin <AAnoshkin@dev.vtb.ru>
2022-02-28 15:01:41 +0300

    add auth by login,password

--- elasticsearch_client.py
+++ elasticsearch_client.py
@@ -1,15 +1,19 @@
 import json
 import re
+from base64 import b64encode
 from datetime import datetime, timedelta, timezone
 from http.client import HTTPConnection
 from time import sleep
-from typing import List
 
 import config
 from config import elasticsearch
 from log_config import get_logger
 
 log = get_logger()
+
+base64_token = b64encode(
+    (config.elasticsearch["USERNAME"] + ":" + config.elasticsearch["PASSWORD"]).encode('ascii')).decode("ascii")
+headers = {"Authorization": "Basic " + base64_token}
 
 
 def is_valid_index_name(index_name_with_date: str) -> bool:
@@ -24,7 +28,7 @@ def is_valid_index_name(index_name_with_date: str) -> bool:
 
 def get_oldest_date_in_indexes() -> datetime:
     connection = HTTPConnection(elasticsearch["HOST"], elasticsearch["PORT"], timeout=45)
-    connection.request("GET", "/_cat/indices?format=json&pretty")
+    connection.request(method="GET", url="/_cat/indices?format=json&pretty", headers=headers)
     response = connection.getresponse().read()
 
     data = json.loads(response)
@@ -47,7 +51,7 @@ def get_indexes_by_date(current_date: datetime) -> list[dict]:
     formatted_date = current_date.strftime("%Y.%m.%d")
 
     connection = HTTPConnection(elasticsearch["HOST"], elasticsearch["PORT"], timeout=45)
-    connection.request("GET", f"_cat/indices/*{formatted_date}?bytes=b&format=json&pretty")
+    connection.request(method="GET", url=f"_cat/indices/*{formatted_date}?bytes=b&format=json&pretty", headers=headers)
     response = str(connection.getresponse().read().decode())
 
     return json.loads(response)
@@ -57,7 +61,8 @@ def get_indexes_by_name(index_name_with_date: str, date_start: datetime, date_en
     log.debug(f"index_name_with_date={index_name_with_date}, date_start={date_start}")
     index_name_without_date = index_name_with_date[:-10]
     connection = HTTPConnection(elasticsearch["HOST"], elasticsearch["PORT"], timeout=45)
-    connection.request("GET", f"_cat/indices/{index_name_without_date}*?s=index&bytes=b&format=json&pretty")
+    connection.request(method="GET", url=f"_cat/indices/{index_name_without_date}*?s=index&bytes=b&format=json&pretty",
+                       headers=headers)
 
     response = str(connection.getresponse().read().decode())
 
@@ -98,9 +103,12 @@ def merge_single_index(index_source: str, index_target: str) -> str:
         }
     }
 
+    custom_headers = dict()
+    custom_headers["Content-Type"] = "application/json"
+    custom_headers["Authorization"] = "Basic " + base64_token
     connection = HTTPConnection(elasticsearch["HOST"], elasticsearch["PORT"], timeout=45)
-    connection.request("POST", "_reindex?wait_for_completion=false", body=json.dumps(body),
-                       headers={"Content-Type": "application/json"})
+    connection.request(method="POST", url="_reindex?wait_for_completion=false", body=json.dumps(body),
+                       headers=custom_headers)
     response = str(connection.getresponse().read().decode())
 
     return json.loads(response)["task"]
@@ -110,7 +118,7 @@ def await_task(task_id: str):
     is_completed = False
     while not is_completed:
         connection = HTTPConnection(elasticsearch["HOST"], elasticsearch["PORT"], timeout=45)
-        connection.request("GET", f"/_tasks/{task_id}")
+        connection.request(method="GET", url=f"/_tasks/{task_id}", headers=headers)
         response = str(connection.getresponse().read().decode())
 
         is_completed = json.loads(response)["completed"]
@@ -128,12 +136,12 @@ def delete_indexes(indexes: list[str]):
 def delete_index(index_name: str):
     log.debug(f"delete index={index_name}")
     connection = HTTPConnection(elasticsearch["HOST"], elasticsearch["PORT"], timeout=45)
-    connection.request("DELETE", f"/{index_name}")
+    connection.request(method="DELETE", url=f"/{index_name}", headers=headers)
 
 
 def get_tmp_indexes() -> list[str]:
     connection = HTTPConnection(elasticsearch["HOST"], elasticsearch["PORT"], timeout=45)
-    connection.request("GET", f"_cat/indices/*_tmp?bytes=b&format=json&pretty")
+    connection.request(method="GET", url=f"_cat/indices/*_tmp?bytes=b&format=json&pretty", headers=headers)
     response = str(connection.getresponse().read().decode())
 
     parsed_response = json.loads(response)
